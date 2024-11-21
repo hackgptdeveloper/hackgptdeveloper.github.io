dc30cdffce9f0e7331bfea84a3c4fc03c3cd2e91
hackgptdeveloper <hackgptdeveloper@gmail.com>
2024-11-21 09:28:16 +0800

    modified:   _config.yml modified:   _includes/disqus.html modified:   _layouts/post.html deleted:    about.md modified:   myrun.sh deleted:    tags.md

--- myrun.sh
+++ myrun.sh
@@ -1,1 +1,7 @@
+
+git config --global user.name "hackgptdeveloper"
+git config --global user.email "hackgptdeveloper@gmail.com"
+git config --global credential.helper store
+
+exit
 docker run -p 4000:4000 -v $(pwd):/site bretfisher/jekyll-serve
